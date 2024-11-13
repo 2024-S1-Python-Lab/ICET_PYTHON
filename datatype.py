5f4e3370697aaf0a24587b5b9e9eed8a449c73b6
Farsanks <farsankadavandy@gmail.com>
2024-11-13 14:42:20 +0530

    hai words

---
+++ datatype.py
@@ -0,0 +1,8 @@
+# a=1
+# print(type(a))
+
+
+name = "farsan"
+print(name[0:4])
+
+print(name[2])
---
+++ wind.py
@@ -0,0 +1,8 @@
+from tkinter import Tk, Label
+
+root = Tk()
+root.title("farsan")
+root.configure(bg='red')
+label = Label()
+root.mainloop()
+
